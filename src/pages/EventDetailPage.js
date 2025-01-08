386f86f01a2d06ef352c7c7b5207ac4a54b3be26
Bellouche Yacine <yacine.bellouche@etudiant.univ-brest.fr>
2025-01-08 23:08:25 +0100

    1

---
+++ src/pages/EventDetailPage.js
@@ -0,0 +1,4 @@
+export default function EventDetailPage() {
+    return <h1>Event Page Detail Page</h1>;
+  }
+  
---
+++ src/pages/NewEventPage.js
@@ -0,0 +1,4 @@
+export default function NewEventPage() {
+    return <h1>New Event Page</h1>;
+  }
+  
